138125053cfd73eec959838a14a9fd54a5a962d7
TomSce <tomda@ac.sce.ac.il>
2019-12-18 19:53:28 +0200

    added course class

---
+++ MVC_project/MVC_project/Models/Course.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace MVC_project.Models
+{
+    public class Course
+    {
+        string Course_ID { set; get; }
+
+        string Lecturer_ID { set; get; }
+
+        Schedule schedule { set; get; }
+
+    }
+}
